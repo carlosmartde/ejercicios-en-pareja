129bdcfa8b0d56fbcda1416df686ec96d405b82f
carlos humberto martinez de leòn <carlosmartde@gmail.com>
2024-02-24 10:02:23 -0600

    ejercicio

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <stdio.h>
 
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int numero;
+    cout << "INGRESE UN NUMERO Y LE DIRE SI ES PAR O IMPAR: " << endl;
+    cin >> numero;
+
+    if (numero%2 ==0){
+        cout<<numero<<" ES UN NUMERO PAR"<<endl;
+    }else {
+        cout<<numero<<" ES UN NUMERO IMPAR"<<endl;
+    }
+
     return 0;
 }
